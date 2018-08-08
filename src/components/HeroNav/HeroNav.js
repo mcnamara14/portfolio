7b332efaedb7f10f89660322682376831939115a
Tyler McNamara <mcnamara14@gmail.com>
2018-08-08 11:10:46 -0600

    Add active class to selected nav link

--- src/components/HeroNav/HeroNav.js
+++ src/components/HeroNav/HeroNav.js
@@ -4,29 +4,44 @@ import { storeProject, activateProject } from '../../actions';
 import './HeroNav.css';
 
 class HeroNav extends Component {
-  selectedProject = (event) => {
+  toggleProject = (event) => {
     const selectedTitle = event.target.closest('div').getElementsByTagName('h6')[0].innerHTML;
+    const selectedDiv = event.target.closest('div');
 
     this.props.projects.forEach(project => {
       const {image, company, title, year } = project;
 
       project.title === selectedTitle ? this.props.activateProject(selectedTitle) : null;
     });
+
+    this.toggleActiveClass(selectedDiv);
+  }
+
+  toggleActiveClass = (selected) => {
+    const heroNavs = document.querySelectorAll('.heroNavLink');
+
+    heroNavs.forEach(nav => {
+      if (nav.classList.contains('active')) {
+        nav.classList.remove('active')
+      }
+    })
+
+    selected.classList.add('active')
   }
 
   render() {
 
     return (
       <section className="heroNav">
-        <div className="heroNavLeft"  id={1}  onClick={(event) => this.selectedProject(event)}>
+        <div className="heroNavLeft heroNavLink active" onClick={(event) => this.toggleProject(event)}>
           <p>01.</p>
           <h6>{ this.props.projects[0].title}</h6>
         </div>
-        <div className="heroNavCenter"  id={2} onClick={(event) => this.selectedProject(event)}>
+        <div className="heroNavCenter heroNavLink" onClick={(event) => this.toggleProject(event)}>
           <p>02.</p>
           <h6>React / Redux Event Planner</h6>
         </div>
-        <div className="heroNavRight" id={3}  onClick={(event) => this.selectedProject(event)}>
+        <div className="heroNavRight heroNavLink" onClick={(event) => this.toggleProject(event)}>
           <p>03.</p>
           <h6>Denver Happy Hour API</h6>
         </div>

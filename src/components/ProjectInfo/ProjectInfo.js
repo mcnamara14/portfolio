caae520d2dabfbc1225eb483d8eca19224a0f507
Tyler McNamara <mcnamara14@gmail.com>
2018-08-10 10:02:16 -0600

    Adjust home info styling and hhh sidebar image

--- src/components/ProjectInfo/ProjectInfo.js
+++ src/components/ProjectInfo/ProjectInfo.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './ProjectInfo.css';
 import evoLiteSide from './images/evo-lite-side.jpg';
+import happyHeroSide from './images/happy-hour-hero-side.jpg';
 
 class ProjectInfo extends Component {
   render() {
@@ -25,6 +26,9 @@ class ProjectInfo extends Component {
       switch (company) {
         case 'Evo-Lite':
           sideImg = evoLiteSide
+          break;
+        case 'Happy Hour Hero':
+          sideImg = happyHeroSide
           break;
         default:
           null

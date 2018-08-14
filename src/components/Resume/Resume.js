581559773bb7927f5bb43c0df2eaba426044ff55
Tyler McNamara <mcnamara14@gmail.com>
2018-08-14 17:57:16 -0600

    Add resume info

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -23,6 +23,29 @@ class Resume extends Component {
           <div className="resumeLeft">
             <h3>Profile</h3>
             <p>Results-driven, entrepreneurial, and innovative software developer with over seven years of web development experience and online marketing management.</p><p>A lifelong love of problem solving, design and technology has led me to a career in my passion, programming.</p>
+            <h3>Technical Skills</h3>
+            <ul>
+              <li>JavaScript / JQuery</li>
+              <li>React / Redux / Router</li>
+              <li>Git / GitHub</li>
+              <li>Testing (Enzyme, Mocha, Chai, Jest)</li>
+              <li>PostgreSQL / Express / Node.js</li>
+              <li>HTML5</li>
+              <li>CSS3 / SASS</li>
+              <li>CMS Development</li>
+              <li>Adobe Suite</li>
+              <li>Branding</li>
+            </ul>
+            <h3>Awards</h3>
+            <p>Two-time winner of the Turing Front-End Code Fair</p>
+            <p>Two-time Turing demo comp finalist</p>
+            <h3>Education</h3>
+            <h4>Turing School of Software & Design</h4>
+            <h5>Graduated 2018</h5>
+            <p>1,500+ hours</p><p>Front-End Engineering Certificate</p>
+            <h4>University of Colorado - Boulder</h4>
+            <h5>Graduated 2017</h5>
+            <p>Bachelors in Marketing</p><p>Certificate in Business Management</p>
           </div>
           <div className="resumeRight">
             <h3>Projects</h3>
@@ -36,6 +59,13 @@ class Resume extends Component {
             <h5>WordPress Website, SEO, Branding, Print Ads, Marketing</h5>
             <p>Developed a custom robust CMS site to match the companies needs. Created the brand, designed all online & print advertisements, & led online marketing efforts to significantly increase traffic.</p>
             <p><strong><em>WordPress / Adobe Suite / AutoCAD</em></strong></p>
+            <h3>Experience</h3>
+            <h4>Evo-Lite <span>/ 2012 - 2017</span></h4>
+            <h5>Digital Marketing Manager</h5>
+            <p>Developed the website for easy maintainability and scalability.</p><p>Lead all digital marketing initiatives including SEO, social media, content & email marketing along with campaign landing page development, branding & product documentation design. </p>
+            <h4>RiNo Design Studio <span>/ 2010 - 2017</span></h4>
+            <h5>Lead Developer</h5>
+            <p>Developed the website for easy maintainability and scalability.</p><p>Lead all digital marketing initiatives including SEO, social media, content & email marketing along with campaign landing page development, branding & product documentation design. </p>
           </div>
         </section>
 
